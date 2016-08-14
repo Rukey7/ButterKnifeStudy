30a16f432e03f881fd4c236f1db39510d06634e1
Rukey7 <deadlong7@gmail.com>
2016-08-14 23:23:10 +0800

    update

--- butterknife-compiler/src/main/java/butterknife/compiler/FieldResourceBinding.java
+++ butterknife-compiler/src/main/java/butterknife/compiler/FieldResourceBinding.java
@@ -5,8 +5,11 @@ package butterknife.compiler;
  */
 public final class FieldResourceBinding {
 
+    // 资源ID
     private final int id;
+    // 字段变量名称
     private final String name;
+    // 获取资源数据的方法
     private final String method;
 
     public FieldResourceBinding(int id, String name, String method) {
